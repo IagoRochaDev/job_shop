05fc297f84bb87e14d81ac6fe1540e0859135226
IagoRochaDev <iagor.oliveira00@gmail.com>
2026-03-18 16:15:41 -0300

    feat: implementada arquitetura do sistema

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "instancia.hpp"
+#include "solucao.hpp"
+#include "solver_JPS.hpp"
+
+int main() {
+    /* TODO */
+
+    return 0;
+}
---
+++ solver_JPS.hpp
@@ -0,0 +1,16 @@
+#include "instancia.hpp"
+#include "solucao.hpp"
+
+class Solver_JPS {
+    Instancia instancia;
+    Solucao solucao;
+
+    Solver_JPS(const Instancia& inst){
+        instancia = inst;
+    }
+
+    bool solver() {
+        /*TODO*/
+        return false;
+    }
+};
