3ab092cfec40a532c59b00306c1b4aefa46c7d9c
IagoRochaDev <iagor.oliveira@gmail.com>
2026-05-04 14:02:39 -0300

    feat: implemnentado classes de digrafo e solucao

--- domain/solucao.hpp
+++ domain/solucao.hpp
@@ -1,6 +1,15 @@
+#pragma once
+
 #include <vector>
 #include <string>
 
-class Solucao{
-    /*TODO*/
+class Solucao {
+public:
+    std::vector<int> ordem_topologica;
+    std::vector<int> caminho_maximo_vertices;
+    float makespan_valor = 0.0f;
+
+    Solucao() = default;
+    explicit Solucao(float makespan)
+        : makespan_valor(makespan) {}
 };
